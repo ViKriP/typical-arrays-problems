3c418fe664b6754298bdd0e942abfd9d04798091
Viktor <viktor_biz@ukr.net>
2020-10-03 22:37:19 +0300

    feat: implement max function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  } else return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
